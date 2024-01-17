b0a49afea76b8baa35798ac37578889ea34f58fa
davidleond99 <davidleond99@gmail.com>
2024-01-17 08:34:03 -0500

    first commit

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import { IoBrowsersOutline, IoCalculator, IoLogoReact } from "react-icons/io5";
+import { SidebarMenuItem } from "./SidebarMenuItem";
+import Link from "next/link";
+
+const meunItems = [
+  {
+    path: "/dashboard/main",
+    icon: <IoBrowsersOutline size={40} />,
+    title: "Dashboard",
+    subtitle: "Visualización",
+  },
+  {
+    path: "/dashboard/counter",
+    icon: <IoCalculator size={40} />,
+    title: "Counter",
+    subtitle: "Contador Client Side",
+  },
+];
+
+export const Sidebar = () => {
+  return (
+    <div
+      id="menu"
+      style={{ width: "400px" }}
+      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen"
+    >
+      <div id="logo" className="my-4 px-6">
+        <h1 className="flex items-centertext-lg md:text-2xl font-bold text-white">
+          <IoLogoReact className="mr-2 mt-1 " />
+          <span>Dash</span>
+          <span className="text-blue-500">8</span>.
+        </h1>
+        <p className="text-slate-500 text-sm">
+          Manage your actions and activities
+        </p>
+      </div>
+      <div id="profile" className="px-6 py-10">
+        <p className="text-slate-500">Welcome back,</p>
+        <Link href="#" className="inline-flex space-x-2 items-center">
+          <span>
+            <Image
+              width={50}
+              height={50}
+              className="rounded-full w-8 h-8"
+              src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c"
+              alt="User avatar"
+            />
+          </span>
+          <span className="text-sm md:text-base font-bold">David León</span>
+        </Link>
+      </div>
+      <div id="nav" className="w-full px-6">
+        {meunItems.map((menuItem) => (
+          <SidebarMenuItem key={menuItem.title} {...menuItem} />
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,8 @@
+
+export default function CounterPage() {
+  return (
+    <div className="flex">
+      <h1>Page Counter</h1>
+    </div>
+  );
+}
