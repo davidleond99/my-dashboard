14b44f8f76427781d066eb27206dd4aa1025b97b
davidleond99 <davidleond99@gmail.com>
2024-01-31 10:19:53 -0500

    Redux toolkit add

---
+++ src/store/pokemons/pokemonsSlice.ts
@@ -0,0 +1,21 @@
+import { SimplePokemon } from "@/pokemons";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface PokemonsState {
+  [key: string]: SimplePokemon;
+}
+
+const initialState: PokemonsState = {
+  "1": { id: "1", name: "bulbasaur" },
+  "2": { id: "2", name: "yvisaur" },
+};
+
+const pokemonsSlice = createSlice({
+  name: "pokemons",
+  initialState,
+  reducers: {},
+});
+
+export const {} = pokemonsSlice.actions;
+
+export default pokemonsSlice.reducer;
